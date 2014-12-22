b37f159ec85e133c360c8055eb79f1afcca079a0
Ron Kuris <swcafe@gmail.com>
2014-12-21 18:38:43 -0800

    Keep IDs across multiple files
    use 00000000 if we never got a map id (still needs debugging)

--- perl/main.pl
+++ perl/main.pl
@@ -6,9 +6,9 @@ use Data::Dumper;
 my %litchr = ( 1=> 'F', 2=>'Fl', 3=>'LFl', 4=>'Q', 5=>'VQ', 6=>'UQ', 7=>'Iso', 8=>'Oc', 9=>'IQ', 10=>'IVQ', 11=>'IUQ', 12=>'Mo', 13=>'FFl', 14=>'FlLFl', 15=>'OcFl', 16=>'FLFl', 17=>'Al.Oc', 18=>'Al.LFl', 19=>'Al.Fl', 20=>'Al.Gr', 25=>'Q', 26=>'VQ', 27=>'UQ', 28=>'Al', 29=>'Al.FFl' );
 my %colour = ( 1=>'white', 2=>'black', 3=>'red', 4=>'green', 5=>'blue', 6=>'yellow', 7=>'grey', 8=>'brown', 9=>'amber', 10=>'violet', 11=>'orange', 12=>'magenta', 13=>'pink');
 
+$id = 0;
 for my $sourcefile (@ARGV) {
     if ( $sourcefile =~ /\.000$/ ) {
-        $id = 0;
         my ( $mapid, $description );
         ( $osm, $osmoutname ) = tempfile("tXXXXXX", DIR => '.', SUFFIX => '.osm' );
         print $osm q/<?xml version='1.0' encoding='UTF-8'?>
@@ -168,10 +168,14 @@ for my $sourcefile (@ARGV) {
         }
         print $osm qq(</osm>\n);
         close($osm);
+	$mapid = '00000000' unless $mapid || -f '00000000.img';
+	$mapid = '00000001' unless $mapid || -f '00000001.img';
+	$mapid = '00000002' unless $mapid || -f '00000002.img';
+
         print
-qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n $mapid --description="$description" $osmoutname\n);
+qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n "$mapid" --description="$description" $osmoutname\n);
         system
-qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n $mapid --description="$description" $osmoutname);
+qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n "$mapid" --description="$description" $osmoutname);
     }
 }
 
