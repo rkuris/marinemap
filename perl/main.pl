b2f7520717939dffd0d27b4d8c83a969fa525437
Ron Kuris <swcafe@gmail.com>
2014-12-21 22:57:52 -0800

    Add lights to beacons

--- perl/main.pl
+++ perl/main.pl
@@ -6,6 +6,11 @@ use Data::Dumper;
 my %litchr = ( 1=> 'F', 2=>'Fl', 3=>'LFl', 4=>'Q', 5=>'VQ', 6=>'UQ', 7=>'Iso', 8=>'Oc', 9=>'IQ', 10=>'IVQ', 11=>'IUQ', 12=>'Mo', 13=>'FFl', 14=>'FlLFl', 15=>'OcFl', 16=>'FLFl', 17=>'Al.Oc', 18=>'Al.LFl', 19=>'Al.Fl', 20=>'Al.Gr', 25=>'Q', 26=>'VQ', 27=>'UQ', 28=>'Al', 29=>'Al.FFl' );
 my %colour = ( 1=>'white', 2=>'black', 3=>'red', 4=>'green', 5=>'blue', 6=>'yellow', 7=>'grey', 8=>'brown', 9=>'amber', 10=>'violet', 11=>'orange', 12=>'magenta', 13=>'pink');
 
+my %catcam = ( 1=>'north', 2=>'east', 3=>'south', 4=>'west');
+my %catlam = ( 1=>'port', 2=>'starboard', 3=>'preferred_channel_starboard', 4=>'preferred_channel_port' );
+my %catlamc = ( 1=>'green', 2=>'red', 3=>'green;red;green', 4=>'red;green;red' );
+my %bcnshp = ( 1=>'pole', 2=>'withy', 3=>'tower', 4=>'lattice', 5=>'pile', 6=>'cairn', 7=>'buoyant' );
+
 $id = 0;
 my ( $osm, $osmoutname ) = tempfile("tXXXXXX", DIR => '.', SUFFIX => '.osm' );
 print $osm q/<?xml version='1.0' encoding='UTF-8'?>
@@ -19,7 +24,61 @@ for my $sourcefile (@ARGV) {
     if ( $sourcefile =~ /\.000$/ ) {
 	# LIGHTS
 	my $lightjson = zerotojson($sourcefile, 'LIGHTS');
-        foreach my $feature ( @{ $lightjson->{'features'} } ) {
+	my %lightmap;
+	# create a map of the light locations so we can search for them when rendering beacons
+	foreach my $feature ( @{ $lightjson->{'features'} } ) {
+	    my $coord = $feature->{'geometry'}->{'coordinates'};
+	    $lightmap{$feature->{'properties'}->{'LNAM'}} = $feature;
+	}
+	my $bcnjson = zerotojson($sourcefile, 'BCNLAT');
+	foreach my $feature ( @{ $bcnjson->{'features'} } ) {
+            my $props = $feature->{'properties'};
+	    my $coord = $feature->{'geometry'}->{'coordinates'};
+		    next if ref $coord->[1];
+		    next if ref $coord->[0];
+		    next unless $coord->[1];
+		    next unless $coord->[0];
+		    next if $coord->[1] < 10;
+
+	    my $tags = " <tag k='seamark:type' v='beacon_lateral' />\n";
+    	    $tags .= " <tag k='seamark:beacon_lateral:category' v='$catlam{$props->{CATLAM}}' />\n" if $props->{CATLAM};
+    	    $tags .= " <tag k='seamark:beacon_lateral:colour' v='$catlamc{$props->{CATLAM}}' />\n" if $props->{CATLAM};
+    	    $tags .= " <tag k='seamark:beacon_lateral:shape' v='$bcnshp{$props->{BCNSHP}}' />\n" if $props->{BCNSHP};
+	    $tags .= " <tag k='seamark:beacon_lateral:ref' v='$props->{OBJNAM}' />\n" if $props->{OBJNAM};
+	    $tags .= " <tag k='seamark:name' v='$1' />\n" if $props->{OBJNAM} =~ /.* ([^ ]*)$/;
+
+	    if ( $props->{'LNAM_REFS'} && ref $props->{'LNAM_REFS'} eq 'ARRAY' ) {
+	    	foreach my $lightref (@{$props->{'LNAM_REFS'}}) {
+		    if (exists $lightmap{$lightref}) {
+			my $light = $lightmap{$lightref};
+                        my $props = $light->{'properties'};
+			delete $lightmap{$lightref};
+	    	        $tags .= "  <tag k='seamark:light:character' v='$litchr{$props->{LITCHR}}' />\n" if ($props->{'LITCHR'} && $litchr{$props->{'LITCHR'}});
+	        	$tags .= "  <tag k='seamark:light:category' v='aero' />\n" if $props->{'CATLIT'} && $props->{'CATLIT'} == 5;
+	        	$tags .= "  <tag k='seamark:light:height' v='$props->{HEIGHT}' />\n" if $props->{'HEIGHT'};
+	        	$tags .= "  <tag k='seamark:light:multiple' v='$props->{MLTYLT}' />\n" if $props->{'MLTYLT'};
+	        	$tags .= "  <tag k='seamark:light:range' v='$props->{VALNMR}' />\n" if $props->{'VALNMR'};
+	    if ($props->{'COLOUR'}) {
+		if ( $props->{'COLOUR'} =~ /,/) {
+		    my $lid = 1;
+		    for my $acolour (split(/,/, $props->{'COLOUR'})) {
+		        $tags .= "  <tag k='seamark:light:$lid:colour' v='$colour{$acolour}' />\n" if $colour{$acolour};
+			$lid++;
+		    }
+		} else {
+		    my $acolour = $props->{'COLOUR'};
+		    $tags .= "  <tag k='seamark:light:colour' v='$colour{$acolour}' />\n" if $colour{$acolour};
+		}
+	    }
+	    $tags .= "  <tag k='seamark:light:group' v='$props->{SIGGRP}' />\n" if $props->{'SIGGRP'};
+	    $tags .= "  <tag k='seamark:light:period' v='$props->{SIGPER}' />\n" if $props->{'SIGPER'};
+	    $tags .= "  <tag k='seamark:light:sequence' v='$props->{SIGSEQ}' />\n" if $props->{'SIGSEQ'};
+		    }
+		}
+	    }
+	    print $osm node(--$id, {'lat'=>$coord->[1], 'lon'=>$coord->[0]}, $tags);
+	}
+        foreach my $feature ( values %lightmap ) {
             my $props = $feature->{'properties'};
 	    my $coord = $feature->{'geometry'}->{'coordinates'};
 		    next if ref $coord->[1];
@@ -163,9 +222,9 @@ for my $sourcefile (@ARGV) {
         print $osm qq(</osm>\n);
         close($osm);
         print
-qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n "$mapid" --description="$description" $osmoutname\n);
+qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n "$mapid" --description="$description" --country-name="USA" --country-abbr="US" --region-name="California" --region-abbr=CA --lower-case --family-name="CA Marine" --max-jobs --copyright-message="NOAA maps are free, does not meet chart carriage regulations"  --license-file=LICENSE $osmoutname\n);
         system
-qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n "$mapid" --description="$description" $osmoutname);
+qq(java -jar mkgmap-r3363/mkgmap.jar --style-file=mkgmap-r3363/styles/rk -n "$mapid" --description="$description" --country-name="USA" --country-abbr="US" --region-name="California" --region-abbr=CA --lower-case --family-name="CA Marine" --max-jobs --copyright-message="NOAA maps are free, does not meet chart carriage regulations"  --license-file=LICENSE $osmoutname);
 
 sub mtofeet
 {
